3d08a1a0adf4511b97c11e7c818cc71be98afc4a
Eugineshole <133373068+Eugineshole@users.noreply.github.com>
2023-07-26 01:07:24 +0200

    Update functions2.c
    group project

--- functions2.c
+++ functions2.c
@@ -9,6 +9,7 @@
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
+ * by Thapelo and Palesa
  * Return: Number of chars printed.
  */
 int print_pointer(va_list types, char buffer[],
